4c15d7524173b817c24cf5139cc68dbbebbe39a2
Trismegiste <flo@trismegiste.net>
2016-10-14 17:12:31 +0200

    properties

--- Character.js
+++ Character.js
@@ -4,9 +4,26 @@
 
 var Character = function () {
     this.currentWounds = 0;
-    this.startToken = 3;
     this.spentToken = 0;
     this.currentFatigue = 0;
-    this.property = [];
+    this.property = {};
+    this.fighting = 0;
+    this.vigor = 4;
+
+
+    this.getMaxToken = function () {
+        return 3 + (this.property['Chanceux'] ? 1 : 0)
+                + (this.property['Très Chanceux'] ? 1 : 0)
+                + (this.property['Malchanceux'] ? -1 : 0)
+                + (this.property['Gamin'] ? 1 : 0);
+    };
+
+    this.getParry = function () {
+        return 2 + Math.floor(this.fighting / 2);
+    };
+
+    this.getToughness = function () {
+        return 2 + Math.floor(this.vigor / 2);
+    }
 };
 
--- app.js
+++ app.js
@@ -4,7 +4,11 @@
 
 var app = {
     init: function () {
-        app.model.push(new Character());
+        var pc = new Character();
+        pc.property["Chanceux"] = true;
+        pc.property["Très Chanceux"] = true;
+        pc.property["Gamin"] = true;
+        app.model.push(pc);
     },
     model: []
 };
