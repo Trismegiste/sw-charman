c30eb46527496203bdc130ceb41f94d267fa0141
Trismegiste <flo@trismegiste.net>
2016-10-14 17:28:51 +0200

    selector

--- Character.js
+++ Character.js
@@ -3,13 +3,13 @@
  */
 
 var Character = function () {
+    this.name = '';
     this.currentWounds = 0;
     this.spentToken = 0;
     this.currentFatigue = 0;
     this.property = {};
     this.fighting = 0;
     this.vigor = 4;
-
 
     this.getMaxToken = function () {
         return 3 + (this.property['Chanceux'] ? 1 : 0)
@@ -24,6 +24,10 @@ var Character = function () {
 
     this.getToughness = function () {
         return 2 + Math.floor(this.vigor / 2);
-    }
+    };
+
+    this.rangedToBeHit = function () {
+        return 4;
+    };
 };
 
--- app.js
+++ app.js
@@ -5,10 +5,25 @@
 var app = {
     init: function () {
         var pc = new Character();
+        pc.name = 'Arkel';
         pc.property["Chanceux"] = true;
         pc.property["Très Chanceux"] = true;
         pc.property["Gamin"] = true;
-        app.model.push(pc);
+        app.addCharacter(pc);
+        // event
+        $('#pc-list').on('change', function (e) {
+            app.model.forEach(function (item) {
+                var search = $('#pc-list').val();
+                if (item.name === search) {
+                    $('#pc-view').html(JSON.stringify(item, null, 2));
+                }
+            });
+        });
+
+    },
+    addCharacter: function (pc) {
+        $('#pc-list').append('<option>' + pc.name + '</option>');
+        this.model.push(pc);
     },
     model: []
 };
