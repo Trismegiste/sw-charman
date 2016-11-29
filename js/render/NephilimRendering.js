94ed2beca3f5b4d171f9adfb41d53c43d88b9573
Trismegiste <flo@trismegiste.net>
2016-11-29 06:06:44 +0100

    removing useless  top margin

--- js/render/NephilimRendering.js
+++ js/render/NephilimRendering.js
@@ -77,7 +77,7 @@ NephilimRendering.prototype.getIdentite = function () {
     title = title.charAt(0).toUpperCase() + title.slice(1)
             + ' '
             + this.character.name.charAt(0).toUpperCase() + this.character.name.slice(1)
-    return {text: title, margin: [0, 6, 0, 6], fontSize: 16}
+    return {text: title, margin: [0, 0, 0, 6], fontSize: 16}
 }
 
 NephilimRendering.prototype.getHistoire = function () {
